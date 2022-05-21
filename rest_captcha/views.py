0276f78b0c21d5d60f7239a7fbd98d267d2d3e5e
6 <skipper224483@gmail.com>
2022-05-22 01:09:56 +0300

    add some docstrings

--- rest_captcha/views.py
+++ rest_captcha/views.py
@@ -8,10 +8,14 @@ cache = caches[api_settings.CAPTCHA_CACHE]
 
 
 class RestCaptchaView(views.APIView):
+    serializer_class = ImageSerializer
     authentication_classes = ()
     permission_classes = ()
 
     def post(self, request):
-        serializer = ImageSerializer()
+        """get captcha image, solve it, then send solution and captcha key to any other endpoint,
+        where captcha is required"""
+
+        serializer = self.serializer_class()
         serializer.is_valid(raise_exception=True)
         return response.Response(serializer.data)
